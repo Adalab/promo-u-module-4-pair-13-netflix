328ad0c034effd77f9625b67e323a4aabf8645c2
Irene García <irenegwodak@gmail.com>
2023-11-02 10:56:46 +0100

    Change call in API to server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2/promise');
 
 // create and config server
 const server = express();
@@ -10,4 +11,39 @@ server.use(express.json());
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+//INIT CONNECTION TO DB
+async function getConnection() {
+  const connection = await mysql.createConnection({
+    host: 'sql.freedb.tech',
+    user: 'freedb_IreMa',
+    password: '3e7xcC4*uxS7p7d',
+    database: 'freedb_NetflixPair',
+  });
+  await connection.connect();
+
+  console.log(
+    `Conexión establecida con la base de datos (identificador=${connection.threadId})`
+  );
+
+  return connection;
+}
+
+//endpoint para todas recibir datos pelis
+server.get('/movies', async (req, res) => {
+  //establecer conexión
+  const connection = await getConnection();
+  //crear consulta
+  let query = 'SELECT * FROM movies';
+  const [results, fields] = await connection.query(query);
+
+  //responder a la petición
+  res.json({
+    success: true,
+    movies: results,
+  });
+
+  //terminar conexión
+  connection.end();
 });
